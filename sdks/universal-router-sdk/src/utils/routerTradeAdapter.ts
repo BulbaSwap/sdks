5319dc7b9903cff9145277a1f284292efd558804
buildtrust <ququzone@qq.com>
2024-07-11 17:06:38 +0800

    replace uniswap package

--- sdks/universal-router-sdk/src/utils/routerTradeAdapter.ts
+++ sdks/universal-router-sdk/src/utils/routerTradeAdapter.ts
@@ -1,7 +1,7 @@
-import { MixedRouteSDK, Trade as RouterTrade } from '@uniswap/router-sdk'
-import { Currency, CurrencyAmount, Ether, Token, TradeType } from '@uniswap/sdk-core'
-import { Pair, Route as V2Route } from '@uniswap/v2-sdk'
-import { Pool, Route as V3Route, FeeAmount } from '@uniswap/v3-sdk'
+import { MixedRouteSDK, Trade as RouterTrade } from '@ququzone/router-sdk'
+import { Currency, CurrencyAmount, Ether, Token, TradeType } from '@ququzone/sdk-core'
+import { Pair, Route as V2Route } from '@ququzone/v2-sdk'
+import { Pool, Route as V3Route, FeeAmount } from '@ququzone/v3-sdk'
 import { BigNumber } from 'ethers'
 import { ETH_ADDRESS, E_ETH_ADDRESS } from './constants'
 
@@ -107,17 +107,17 @@ export class RouterTradeAdapter {
       return {
         routev3: isOnlyV3
           ? new V3Route(
-              (subRoute as V3PoolInRoute[]).map(RouterTradeAdapter.toPool),
-              parsedCurrencyIn,
-              parsedCurrencyOut
-            )
+            (subRoute as V3PoolInRoute[]).map(RouterTradeAdapter.toPool),
+            parsedCurrencyIn,
+            parsedCurrencyOut
+          )
           : null,
         routev2: isOnlyV2
           ? new V2Route(
-              (subRoute as V2PoolInRoute[]).map(RouterTradeAdapter.toPair),
-              parsedCurrencyIn,
-              parsedCurrencyOut
-            )
+            (subRoute as V2PoolInRoute[]).map(RouterTradeAdapter.toPair),
+            parsedCurrencyIn,
+            parsedCurrencyOut
+          )
           : null,
         mixedRoute:
           !isOnlyV3 && !isOnlyV2
