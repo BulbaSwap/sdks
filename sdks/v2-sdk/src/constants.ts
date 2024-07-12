f9fc98d3f2d96b74e166cbed634c6ffb7e386207
buildtrust <ququzone@qq.com>
2024-07-12 09:21:28 +0800

    publish holesky version

--- sdks/v2-sdk/src/constants.ts
+++ sdks/v2-sdk/src/constants.ts
@@ -8,7 +8,9 @@ export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+//export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+
+export const INIT_CODE_HASH = '0x1140ae914d176ac1defdd79292e8a309bf49f7c645ac011435c6e8b75dff6b64'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
